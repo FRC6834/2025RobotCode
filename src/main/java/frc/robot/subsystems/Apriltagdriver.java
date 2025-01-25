e39cbe139dfd1a0e621774738ab7231437858349
prestonGarvey1 <thebozzkid2010@gmail.com>
2025-01-25 00:03:09 -0500

    Make Apriltag 21 turn on  the light

--- src/main/java/frc/robot/subsystems/Apriltagdriver.java
+++ src/main/java/frc/robot/subsystems/Apriltagdriver.java
@@ -6,7 +6,15 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Apriltagdriver {
     final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tagID = table.getEntry("tid");
+   NetworkTableEntry tagID =  table.getEntry("tid"); //stupid syntax error that makes no sense; ill fix later
+
+    if (tagID == 21) {
+        // Turn on light 
+        table.getEntry("ledMode").setNumber(3);  // 3 means turn on LEDs
+    } else {
+        // Turn off light
+        table.getEntry("ledMode").setNumber(1);  // 1 means turn off LEDs
+    }
 }
 
 
