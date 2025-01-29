2bc3c538e14ba6de16eb7f3698a5dd96fcedb839
prestonGarvey1 <thebozzkid2010@gmail.com>
2025-01-29 13:10:58 -0500

    make team 6834 great again

--- src/main/java/frc/commands/IntakeCommand.java
+++ src/main/java/frc/commands/IntakeCommand.java
@@ -3,7 +3,7 @@ package frc.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveSubsystem;
 
-public class ElevatorCommand extends Command{
+public class IntakeCommand extends Command{
 
   //yo whoever on elevator team make it stop at each coral level too
      //called when the command is initialized
@@ -17,6 +17,6 @@ public class ElevatorCommand extends Command{
      public void end(boolean interrupted){}
  
      @Override
-     public boolean isFinished(){}
+     public boolean isFinished(){return false;}
  }
  
