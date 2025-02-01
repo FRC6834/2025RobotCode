a1be3b935fe6b22770387282b4c97036617748d3
mm2139 <maryam@sokomali.com>
2025-02-01 10:55:29 -0500

    fixed command names
    lalala

--- src/main/java/frc/commands/IntakeInCommand.java
+++ src/main/java/frc/commands/IntakeInCommand.java
@@ -3,7 +3,7 @@ package frc.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveSubsystem;
 
-public class IntakeCommand extends Command{
+public class IntakeInCommand extends Command{
 
   //yo whoever on elevator team make it stop at each coral level too
      //called when the command is initialized
