8a79bb89fb77c50b705cd87729469c6a8ef999b6
prestonGarvey1 <thebozzkid2010@gmail.com>
2025-02-10 10:17:17 -0500

    add PhotonDriver and update dependancies, this will break code for now but will be fixed

---
+++ src/main/java/frc/robot/subsystems/PhotonDriver.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.photonvision.
+public class PhotonDriver {
+    public static void rpi1(){
+        final NetworkTable table = NetworkTableInstance.getDefault().getTable("photonvision");
+        final double TagID  =  table.getEntry("tid").getDouble(0);
+    }
+    
+}
