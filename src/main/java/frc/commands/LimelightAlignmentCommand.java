1abbfa2adae550c07fdaa5164d21f1e1631e09c0
Zephyros <zxphyros@gmail.com>
2025-02-22 16:06:22 -0500

    limelight

--- src/main/java/frc/commands/LimelightAlignmentCommand.java
+++ src/main/java/frc/commands/LimelightAlignmentCommand.java
@@ -1,23 +1,27 @@
 package frc.commands;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.LimelightSubsystem;
 
 public class LimelightAlignmentCommand extends Command{
     //called when the command is initialized
-//final private DriveSubsystem drive = new DriveSubsystem();
+    final private DriveSubsystem drive = new DriveSubsystem();
+    final private LimelightSubsystem subsystem = new LimelightSubsystem();
+    private double driveDistance;
 
     @Override
     public void initialize(){
-
-        //note to self test the distance before u test angle
-        LimelightSubsystem.align();
-        //final double distance = HELPER.getDistance();
+        double distance = subsystem.getDistance()/39.37; //convert to meters
+        new DriveCommand(drive, distance, 1, 1, 0);
     }
     //called every time the command is scheduled
     @Override
     public void execute(){
-
+        final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        driveDistance = table.getEntry("ty").getDouble(-1);
     }
 
     @Override
@@ -27,6 +31,6 @@ public class LimelightAlignmentCommand extends Command{
 
     @Override
     public boolean isFinished(){
-        return false;
+        return (driveDistance>-1&&driveDistance<1); //ends when ty is between -1 and 1 instead of only ending if ty is 0 in case the ty is 0.01 instead of 0 or smth - alexander 2/22
     }
 }
