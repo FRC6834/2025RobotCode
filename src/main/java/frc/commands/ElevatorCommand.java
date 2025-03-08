2669eaa0a1fedf4e111ff3eded007eeb88dbecaf
mm2139 <147198709+mm2139@users.noreply.github.com>
2025-03-08 11:55:58 -0500

    elevator
    organizing elevator commands and subsystem

--- src/main/java/frc/commands/ElevatorCommand.java
+++ src/main/java/frc/commands/ElevatorCommand.java
@@ -1,29 +1,26 @@
 package frc.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.Constants.ElevatorConstants;
 
 public class ElevatorCommand extends Command{
-
-  final private ElevatorSubsystem subsystem = new ElevatorSubsystem();
-
-
 
   //called when the command is initialized
   @Override
   public void initialize(){ 
+    ElevatorSubsystem.moveToSetpoint(ElevatorConstants.Levels.home);
   }
   
   //called every time the command is scheduled
   @Override
   public void execute(){
-    subsystem.moveToSetpoint(Constants.ElevatorConstants.targetHeight);
+
   }
  
   @Override
   public void end(boolean interrupted){
-    subsystem.stopElevator();
+    ElevatorSubsystem.stopElevator();
   }
   
   //command end condition. returning true ends the command so u can choose when it stops if u want
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -17,36 +17,37 @@ import frc.robot.Constants.ElevatorConstants;
 public class ElevatorSubsystem extends SubsystemBase {
   
   // initializions
-  private final SparkMax kElevatorSubsystemMain = new SparkMax(ElevatorConstants.kElevatorSubsystemMain, MotorType.kBrushless);
-  private final SparkMax kElevatorSubsystemFollower = new SparkMax(ElevatorConstants.kElevatorSubsystemFollower, MotorType.kBrushless);
-  public double elevatorSprocketCircumference = 2* Math. PI * 1.5; // 2/15/2025 this is in inches. also the 1.5 is just a placeholder number for the radius of the sprocket, this will need to be remeasured
+  private static final SparkMax kElevatorSubsystemMain = new SparkMax(ElevatorConstants.kElevatorSubsystemMain, MotorType.kBrushless);
+  private static final SparkMax kElevatorSubsystemFollower = new SparkMax(ElevatorConstants.kElevatorSubsystemFollower, MotorType.kBrushless);
+  public static double elevatorSprocketCircumference = 2* Math. PI * 1.5; // 2/15/2025 this is in inches. also the 1.5 is just a placeholder number for the radius of the sprocket, this will need to be remeasured
+  public static double targetHeight;
 
   //PID controller
-  PIDController elevatorPID = new PIDController(0.1, 0.0, 0.0); // 2/1/2025 need 2 be determined with testing
-  private RelativeEncoder elevatorEncoder = kElevatorSubsystemMain.getEncoder();
+  private static PIDController elevatorPID = new PIDController(0.1, 0.0, 0.0); // 2/1/2025 need 2 be determined with testing
+  private static RelativeEncoder elevatorEncoder = kElevatorSubsystemMain.getEncoder();
 
   // moving elevator to setpoint
-  public void moveToSetpoint(double targetHeight){
+  public static void moveToSetpoint(double targetHeight){
     double setpoint = targetHeight/(2 * Math.PI * elevatorSprocketCircumference); // 2/22/2025 dividing the height of the reefLevel we want to go to by the circumfrence of the elevator sprocket to figure out what the target number of rotations is, which is the setPoint
     double pidDifference = elevatorPID.calculate(elevatorEncoder.getPosition(), setpoint); 
     kElevatorSubsystemMain.set(pidDifference); 
     kElevatorSubsystemFollower.set(pidDifference); 
   }
   
- public void setConstants(double reefLevel){
-     Constants.ElevatorConstants.targetHeight = reefLevel;
+ public static void setConstants(double reefLevel){
+     targetHeight = reefLevel;
     }
 
 
-     public void elevatorUp(){
+     public static void elevatorUp(){
       kElevatorSubsystemMain.set(-.75);
      }
   
-     public void elevatorDown() {
+     public static void elevatorDown() {
       kElevatorSubsystemMain.set(.75);
      }
 
-     public void stopElevator(){
+     public static void stopElevator(){
       kElevatorSubsystemMain.set(0);
     }
 
